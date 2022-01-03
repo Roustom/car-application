e6f6c46bb4610be72cd17b54f523528b9d1b3603
Roustom <47872392+Roustom@users.noreply.github.com>
2022-01-03 05:52:34 -0500

    created the welcome page

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button, TouchableOpacity } from 'react-native';
+// import { Button } from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={styles.imageContainer}>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.titleText}>Car Detailing</Text>
+        <Text style={styles.bodyText}>We detail your vehicle anytime in the year from the comfort of your home!</Text>
+        <TouchableOpacity
+          style={styles.loginScreenButton}
+          onPress={() => navigate('HomeScreen')}
+          underlayColor='#fff'>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.signupScreenButton}
+          onPress={() => navigate('HomeScreen')}
+          underlayColor='#fff'>
+          <Text style={styles.signupText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  mainContainer: {
+    flexDirection: 'column',
+    // marginTop: 500,
+    flex: 2,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer: {
+    flexDirection: 'column',
+    flex: 2,
+    backgroundColor: '#0059d4',
+  },
+  buttonContainer: {
+    paddingTop:0,
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: '#006bff',
+  },
+  loginScreenButton: {
+    marginRight:40,
+    marginLeft:40,
+    marginTop:40,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'white',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  loginText:{
+    color:'#006bff',
+    textAlign:'center',
+    paddingLeft : 10,
+    paddingRight : 10
+  },
+  signupScreenButton: {
+    marginRight:40,
+    marginLeft:40,
+    marginTop:20,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#006bff',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  signupText:{
+    color:'white',
+    textAlign:'center',
+    paddingLeft : 10,
+    paddingRight : 10
+  },
+  titleText:{
+    marginLeft:40,
+    marginTop:30,
+    fontSize:30,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  bodyText: {
+    marginLeft: 40,
+    marginTop:10,
+    marginRight: 40,
+    color: 'white'
+  }
 });
